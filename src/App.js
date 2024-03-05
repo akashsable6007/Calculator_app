4089297495a0e86d33d2714f9d2ff5ec3a7d70df
Your Name <you@example.com>
2024-03-05 21:40:38 +0530

    calapp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {useState} from 'react';
+
 
 function App() {
+
+const [expression , setExpression] = useState('');
+
+const clickHandler = (value) => {
+ if (value === '=') {
+  try {
+    setExpression(eval(expression).toString());
+  } catch (error) {
+    setExpression('error')
+  }
+ } else if (value === "C") {
+  setExpression('')
+ }
+ else{
+  setExpression(expression + value);
+ }
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="calculator">
+      <h3>Calculator App</h3>
+      <input type="text"
+             className='form-control expression-display'
+             readOnly value={expression}
+            placeholder='0'
+            />
+
+      <div className="buttons">
+        <button className="btn btn-primary" onClick={() => clickHandler('1')} >1</button>
+        <button className="btn btn-primary" onClick={() => clickHandler('2')} >2</button>
+        <button className="btn btn-primary" onClick={() => clickHandler('3')} >3</button>
+        <button className="btn btn-primary" id='plusoperator' onClick={() => clickHandler('+')} >+</button>
+        <button className="btn btn-primary" onClick={() => clickHandler('4')} >4</button>
+        <button className="btn btn-primary" onClick={() => clickHandler('5')} >5</button>
+        <button className="btn btn-primary" onClick={() => clickHandler('6')} >6</button>
+        <button className="btn btn-primary" id='minusoperator' onClick={() => clickHandler('-')} >-</button>
+        <button className="btn btn-primary" onClick={() => clickHandler('7')} >7</button>
+        <button className="btn btn-primary" onClick={() => clickHandler('8')} >8</button>
+        <button className="btn btn-primary" onClick={() => clickHandler('9')} >9</button>
+        <button className="btn btn-primary" id='muiltioperator' onClick={() => clickHandler('*')} >*</button>
+        <button className="btn btn-primary" id='caloperator' onClick={() => clickHandler('C')} >C</button>
+        <button className="btn btn-primary" onClick={() => clickHandler('0')} >0</button>
+        <button className="btn btn-primary" id='equaloperator' onClick={() => clickHandler('=')} >=</button>
+        <button className="btn btn-primary" id='divoperator' onClick={() => clickHandler('/')} >/</button>
+      </div>
     </div>
+    
+    
+    </>
   );
 }
 
